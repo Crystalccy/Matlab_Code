c0ff8963b1052a09a43197ff5e46edc23292470e
dongxuewang <dongxue.wang1988@gmail.com>
2014-07-02 21:56:36 +0200

    add prospRecons.m and prospProjection.m

---
+++ Matlab/reconstruction/porspProjction.m
@@ -0,0 +1,31 @@
+function result = porspProjction(IMG,angle,D_cam)
+f = 30;
+[Width,Height] = size(IMG);
+Diag = ceil(sqrt(Width^2 + Height^2));
+Pad_Width = ceil((Diag-Width)/2);
+Pad_Height = ceil((Diag-Height)/2);
+Pad_IMG = padarray(IMG,[Pad_Width+2,Pad_Height+2],'both');
+Len_angle = length(angle);
+result.trans = zeros(Len_angle,size(Pad_IMG,2));
+%result.trans = zeros(size(Pad_IMG,1),size(Pad_IMG,2));
+for i = 1:Len_angle
+    Temp_IMG = imrotate(Pad_IMG,angle(i),'bilinear','crop');
+    for row = 1:size(Pad_IMG,1)
+        for coloum = 1:size(Pad_IMG,2)
+            pos = f*(coloum-1-size(Pad_IMG,2)/2)/(D_cam(i)+row-1)+size(Pad_IMG,2)/2;
+            if row == 1
+                result.pos(coloum) = pos;
+            end
+            
+            if rem(pos,1)==0
+                %result.trans(row,result.pos(coloum)) = result.trans(row,result.pos(coloum))+Temp_IMG(row,coloum);
+                result.trans(i,pos) = result.trans(i,pos)+Temp_IMG(row,coloum);
+            end
+            
+        end
+    end
+    reuslt.angle(i) = angle(i);
+    reuslt.Camey(i) = D_cam(i);
+    i
+end
+result.trans = result.trans';
---
+++ Matlab/reconstruction/prospRecons.m
@@ -0,0 +1,82 @@
+function result = prospRecons(IMG,Angle,D_cam)
+f = 30;
+pad = D_cam-f;
+pad_IMG = padarray(IMG,[pad,pad],'both');
+len1 = size(IMG,1);
+len2 = size(pad_IMG,1);
+result = zeros(len2,len2);
+temp_result = result;
+mid = ceil(len1/2);
+for i = 1:length(Angle)
+    Line = IMG(:,i);
+    Pos = find(Line~=0);
+    for row = 1:len2
+        Px = ceil((Pos-mid)*(f+row-1)/f+mid);
+        P = find((Px+pad)>0 & (Px+pad)<len2);
+        % temp_result(row,P)
+        temp_result(row,Px(P)+pad) = Line(Pos(P));
+    end
+    delaAndero = prosDehole(temp_result);
+    %Len = size(delaAndero,1);
+    %delaAndero1 = myfilter(delaAndero,Len);
+
+    result = result + imrotate(delaAndero,-Angle(i),'bilinear','crop');
+    i
+end
+end
+
+function result = prosDehole(IMG1)
+PlusIMG_pad = padarray(IMG1,[10,10],'both');
+[row1, coloum1] = size(PlusIMG_pad);
+dela = delation(PlusIMG_pad,row1,coloum1);
+ero = erosion(dela,row1,coloum1);
+result = ero(11:end-10,11:end-10);
+end
+
+%%  delation
+function result = delation(img1,row1,coloum1)
+result_pad = zeros(size(img1));
+img1_temp = img1;
+time = 5;
+mask1 = [0 1 0;1 1 1;0 1 0];
+while time ~= 0
+    for i = 2:row1-1
+        for j = 2:coloum1-1
+            check_block = img1(i-1:i+1,j-1:j+1);
+            check = check_block.*mask1;
+            if  sum(sum(check))~=0
+                img1_temp(i,j)=2;
+                temp_block = result_pad(i-1:i+1,j-1:j+1);
+                result_pad(i,j) = sum(sum(temp_block))/sum(sum(temp_block~=0));
+            end
+        end
+    end
+    img1 = img1_temp;
+    time = time-1;
+end
+result = img1;
+end
+
+%% erosion
+function result = erosion(img2,row1,coloum1)
+result_pad = zeros(size(img2));
+mask1 = [0 1 0;1 1 1;0 1 0];
+img2_temp = img2;
+time = 5;
+while time ~= 0
+    for i = 2:row1-1
+        for j = 2:coloum1-1
+            check_block = img2(i-1:i+1,j-1:j+1);
+            check = check_block.*mask1;
+            if  (check(1,2)==0 ||check(2,1)==0||check(2,2)==0||check(2,3)==0||check(3,2)==0)
+                img2_temp(i,j)=0;
+                temp_block = result_pad(i-1:i+1,j-1:j+1);
+                result_pad(i,j) = 0;
+            end
+        end
+    end
+    time = time-1;
+    img2 = img2_temp;
+end
+result = img2;
+end
